61c08ed89d3d00dbb3b8dd290cc2af32bb732d45
Jack <jdross@albany.edu>
2025-02-26 12:55:55 -0500

    added camera switch button

--- lib/Pages/camera_page.dart
+++ lib/Pages/camera_page.dart
@@ -13,14 +13,31 @@ class CameraPage extends StatefulWidget {
 class _CameraPageState extends State<CameraPage> {
   late CameraController _controller;
   late Future<void> _initControllerFuture;
-
+  int _currentCameraIndex = 0;
+  
   @override
   void initState() {
     super.initState();
+    // initialize camera (back facing camera by default)
+    _initializeCamera(_currentCameraIndex);
+  }
+
+  // initialize the camera based on index
+  Future<void> _initializeCamera(int cameraIndex) async {
+    final camera = widget.cameras[cameraIndex];
     _controller = CameraController(
-        widget.cameras[0], ResolutionPreset.medium
+      camera,
+      ResolutionPreset.medium,
     );
     _initControllerFuture = _controller.initialize();
+    setState(() {});
+  }
+
+  // switch between the front and back camera
+  void _switchCamera() async {
+    _currentCameraIndex = (_currentCameraIndex + 1) % widget.cameras.length;
+    await _controller.dispose();
+    _initializeCamera(_currentCameraIndex);
   }
 
   void onCameraPressed() async {
@@ -39,37 +56,47 @@ class _CameraPageState extends State<CameraPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: FutureBuilder(
-          future: _initControllerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done){
-              return Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  CameraPreview(_controller),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        onPressed: onCameraPressed,
-                        iconSize: 50,
-                        icon: Icon(Icons.camera),
-                      ),
-
-                      IconButton(
-                          onPressed: onPhotoLibraryPressed,
-                          iconSize: 50,
-                          icon: Icon(Icons.photo_album)
-                      )
-                    ],
-                  )
-                ],
-              );
-            } else {
-              return Center(child: CircularProgressIndicator());
-            }
-          })
+        future: _initControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CameraPreview(_controller),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: onCameraPressed,
+                      iconSize: 50,
+                      icon: Icon(Icons.camera),
+                    ),
+                    IconButton(
+                      onPressed: onPhotoLibraryPressed,
+                      iconSize: 50,
+                      icon: Icon(Icons.photo_album),
+                    ),
+                  ],
+                ),
+                IconButton(
+                  onPressed: _switchCamera,
+                  icon: Icon(Icons.refresh),
+                  iconSize: 50,
+                  color: Colors.blue,
+                ),
+                Text(
+                  _currentCameraIndex == 0 ? 'Back Camera' : 'Front Camera',
+                  style: TextStyle(fontSize: 16),
+                )
+              ],
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
     );
   }
 }
